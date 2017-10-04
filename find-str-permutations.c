3bb090a2e7a9be0bfbc35b9904d2c00f1d9497a8
Yehuda Y <yehuda80@gmail.com>
2017-10-04 06:13:25 +0300

    add template for find string permutation questions
    Signed-off-by: Yehuda Y <yehuda80@gmail.com>

---
+++ find-str-permutations.c
@@ -0,0 +1,9 @@
+/* the idea is to find if a string or any of its permutations were inserted previously to our data structure */
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	printf("Hello world\n");	
+
+}
